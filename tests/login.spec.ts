ca8945df02ddf31f1c4e70d875f0762d709dea93
Thalyson Santos <thalyson.ufla@gmail.com>
2026-01-13 15:52:58 -0300

    test: adiciona testes de carrinho - adicionar e remover itens

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -31,5 +31,29 @@ test.describe('Login', () => {
 
     await expect(page.locator('[data-test="error"]')).toContainText('Username is required');
   });
+
+  test('deve adicionar produto ao carrinho', async ({page}) =>{
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1')
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    await expect(page.locator('[data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
+  });
+
+  test('deve remover produto do carrinho', async ({page}) =>{
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+    
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    await expect(page.getByText('Sauce Labs Backpack')).not.toBeVisible();
+    await expect(page.locator('.shopping_cart_badge')).not.toBeVisible();
+  });
 });
 
